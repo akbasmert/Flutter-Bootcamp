fb5e1c32f333c1987ca914b433ec7f8318eca148
mert <mert.ma346@gmail.com>
2022-12-06 16:07:55 +0300

    Ödev bir tamamladi.

---
+++ lib/odevler/odev_bir.dart
@@ -0,0 +1,58 @@
+
+String sehir = "Ordu";
+String ulke = "Türkiye";
+String email = "mert.ma246@gmail.com";
+String meslek = "Elektrik Elektronik Mühendisi";
+String musteriAdi = "Hilal";
+String dogumGunu = "11/05/1999";
+String urunYorum = "Comment";
+double bakiye = 44.5;
+double maas = 20000.0;
+bool medeniDurum = false;
+int telefon = 11111111111;
+int postaKodu = 52200;
+int stokMiktari = 455;
+
+
+String odemeTarihi = "11/05/2022";
+String arabaModeli = "3.20i msport";
+String kitapAdi = "Oblomow";
+String yayinlanmaTarihi = "--/--/----";
+String enlem = "45.4";
+String boylam = "45.4";
+String urunAdi = "Araba";
+String yemekTarifi = "yağ, salça, soğan üçlüsüne ne katarsanız :)";
+double odeme = 200.2;
+int indirimMiktairi = 30; //Yüzde 30 olarak.
+int siparisAdeti = 200;
+int odaSayisi = 4;
+
+
+String marka = "BMW";
+String muzikAdi = "Elif Dedim";
+String resimAdi = "Kaplumbağa Terbiyecisi";
+String dosyaFormati = "mp3";
+String renk = "Beyaz";
+String renkKodu = "255,255,255";
+String telefonModeli = "iphone 12";
+double videoSuresi = 5.43;
+double urunPuani = 4.6;
+double ekranBoyutu = 6.1;
+double agirlik = 220.0;
+
+
+String ulusalGun = "29 Ekim";
+String tatilGunu = "Ramazan Bayramı";
+String rezervasyonTarihi = "11/07/2023";
+String sokakAdi = "1001 Sokak";
+String otobusHatti = "61A";
+String kuponKodu = "FGH5454";
+String faturaAdresi = "Karşıyaka mah. Altınordu/ORDU";
+String takipKodu = "ASD52200";
+int kalanDakika = 52;
+int kuponSuresi = 30;
+
+
+
+
+
